7cb78b1a98c4737ab979fdaa118afef90e49f5ef
bje16002 <36021778+bje16002@users.noreply.github.com>
2018-02-01 18:14:40 -0800

    01 Feb 2018 : Created project. 01 Feb 2018 : Modified Project

---
+++ OregonTrail/src/oregontrail/model/Game.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oregontrail.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Mike Bjerke & Melissa Holland
+ */
+public class Game implements Serializable { 
+    
+    // class variables
+    private int gameDate;
+    private int mileage;
+    private Player player;
+
+    public int getGameDate() {
+        return gameDate;
+    }
+
+    public void setGameDate(int gameDate) {
+        this.gameDate = gameDate;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+    
+    
+}
