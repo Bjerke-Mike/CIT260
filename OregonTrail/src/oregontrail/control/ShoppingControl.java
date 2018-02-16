c28af8f21c1d0858bb760fedec1b04bcfab0ad5c
bje16002 <36021778+bje16002@users.noreply.github.com>
2018-02-15 16:51:11 -0800

    01 Feb 2018 : Created project. 02 Feb 2018 : Modified Project

--- OregonTrail/src/oregontrail/control/ShoppingControl.java
+++ OregonTrail/src/oregontrail/control/ShoppingControl.java
@@ -7,28 +7,30 @@ package oregontrail.control;
 
 /**
  *
- * @author mellissah.
+ * @author Mellisa Holland & Mike Bjerke.
  */
 public class ShoppingControl {
     
-    public double calculatePrice( int position, String inventoryItem) {
-        return 0;
-        /*IF (position > 5) THEN
-            RETURN -1.0
-          IF (position < 1) THEN
-            RETURN -2.0
-          IF (inventoryItem ≠ “food” AND inventoryItem ≠ “ammo” AND inventoryItem ≠ “clothing”
-                AND inventoryItem ≠ “misc”) THEN
-            RETURN -3.0
-          basePrice = 0.40
-          IF (inventoryItem = “ammo”) THEN
+    static public double calculatePrice(int position, String inventoryItem) {
+        if (position > 5) {
+            return -1.0; }
+        if (position < 1) {
+            return -2.0; }
+        if ( !(inventoryItem == "food" || inventoryItem == "ammo"
+                || inventoryItem == "clothing" || inventoryItem == "misc")) {
+            return -3.0; }
+        /*
+        basePrice = 0.40
+        IF (inventoryItem = “ammo”) THEN
             basePrice = 0.80
-          IF (inventoryItem = “clothing”) THEN
+        IF (inventoryItem = “clothing”) THEN
             basePrice = 1.20
-          IF (inventoryItem = “misc”) THEN
+        IF (inventoryItem = “misc”) THEN
             basePrice = 1.00
-          totalPrice = position * 0.10 + basePrice
-          RETURN totalPrice*/
+        totalPrice = position * 0.10 + basePrice
+        RETURN totalPrice
+        */
+        return 0.0;
         
         
     }
