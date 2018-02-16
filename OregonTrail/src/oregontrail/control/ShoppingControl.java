a1d45d3be775b8352cca03a7e77195aba55c9cb8
mellissah. <mellissah.@192.168.0.6>
2018-02-15 16:31:24 -0800

    Merge origin/master
    Conflicts:
    	OregonTrail/src/oregontrail/OregonTrail.java

---
+++ OregonTrail/src/oregontrail/control/ShoppingControl.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oregontrail.control;
+
+/**
+ *
+ * @author mellissah.
+ */
+public class ShoppingControl {
+    
+    public double calculatePrice( int position, String inventoryItem) {
+        /*IF (position > 5) THEN
+            RETURN -1.0
+          IF (position < 1) THEN
+            RETURN -2.0
+          IF (inventoryItem ≠ “food” AND inventoryItem ≠ “ammo” AND inventoryItem ≠ “clothing”
+                AND inventoryItem ≠ “misc”) THEN
+            RETURN -3.0
+          basePrice = 0.40
+          IF (inventoryItem = “ammo”) THEN
+            basePrice = 0.80
+          IF (inventoryItem = “clothing”) THEN
+            basePrice = 1.20
+          IF (inventoryItem = “misc”) THEN
+            basePrice = 1.00
+          totalPrice = position * 0.10 + basePrice
+          RETURN totalPrice*/
+        
+        
+    }
+}
