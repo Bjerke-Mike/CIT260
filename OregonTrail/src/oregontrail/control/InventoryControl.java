a29cb667ad7064481d82ba872f311b94fc84ba75
mellissah. <mellissah.@192.168.0.6>
2018-02-15 21:46:34 -0800

    Merge origin/master
    Conflicts:
    	OregonTrail/src/oregontrail/OregonTrail.java

--- OregonTrail/src/oregontrail/control/InventoryControl.java
+++ OregonTrail/src/oregontrail/control/InventoryControl.java
@@ -10,5 +10,27 @@ package oregontrail.control;
  * @author mellissah.
  */
 public class InventoryControl {
+   
+    public double addToIventory(String inventoryItem, int amount) {
+       
+        if (amount > 300) {
+            return 0; }
+        if (amount < 1) {
+            return -10; }
+        if ( !(inventoryItem == "food" || inventoryItem == "ammo"
+                || inventoryItem == "clothing" || inventoryItem == "misc")) {
+            return -20; }
+    
+        double itemUnits = 50;
+        if (inventoryItem == "food") {
+           itemUnits = 150; }
+        if (inventoryItem == "ammo") {
+           itemUnits = 50; }
+        if (inventoryItem == "clothing") {
+           itemUnits = 50; }
+        if (inventoryItem == "misc") {
+           itemUnits = 50; }
+        double totalInventory = (itemUnits * amount);
+        return totalInventory; }
     
 }
