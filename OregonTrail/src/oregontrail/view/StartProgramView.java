8ce1eddea28dfda28a28dce5532108d500ad750d
mellissah. <mellissah.@192.168.0.6>
2018-02-22 16:38:18 -0800

    Merge origin/master
    Conflicts:
    	OregonTrail/src/oregontrail/OregonTrail.java

--- OregonTrail/src/oregontrail/view/StartProgramView.java
+++ OregonTrail/src/oregontrail/view/StartProgramView.java
@@ -13,6 +13,20 @@ import oregontrail.model.Player;
  */
 public class StartProgramView {
     public void displayStartProgramView(){
+        /*
+        endOfView = false
+        DO
+            inputs = getInputs()
+            IF no inputs were entered OR the first input is Q
+                RETURN
+            ENDIF
+            endOfView = doAction(inputs)
+        WHILE endOfView != true 
+        */
+        String[] inputs = this.getInputs();
+    }
+        
+        /*
         displayBanner();
         String playerName = null;
         do {
@@ -28,7 +42,8 @@ public class StartProgramView {
         player.setName(playerName);
         
         return;
-    }
+        */
+        
     
     private void displayBanner(){
         System.out.println("Welcome to the Oregon Trail.\nWould you like instructions? (Y/N)");
@@ -39,5 +54,21 @@ public class StartProgramView {
         return "Melissa and Mike";
     }
     
-    
+   private String [] getInputs() {
+       System.out.println("*** getInputs called ***");
+       
+       String[] inputs = new String[1];
+       inputs[0] = "testInput";
+       
+       return inputs;        
+   } 
+   
+   private boolean doAction(String[] inputs) {
+       System.out.println("*** doAction called ***");
+       System.out.println("\tinputs " + inputs[0]);
+       
+       return true;
+   }
+   
 }
+
