e623eb3b0f86854ca34e0d010ba795fbfd0509c0
bje16002 <36021778+bje16002@users.noreply.github.com>
2018-02-23 19:58:04 -0800

    01 Feb 2018 : Created project. 02 Feb 2018 : Modified Project

---
+++ OregonTrail/src/oregontrail/view/HelpMenuView.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oregontrail.view;
+
+/**
+ *
+ * @author Mike
+ */
+class HelpMenuView {
+    public void displayHelpMenuView() {
+        System.out.println("*** displayHelpMenuView() called***");
+    }
+}
