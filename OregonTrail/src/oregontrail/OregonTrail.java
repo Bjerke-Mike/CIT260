d17a0df6fd75958a2dd930cce6eec9eede041417
mellissah. <mellissah.@192.168.0.6>
2018-02-23 17:56:44 -0800

    Merge origin/master
    Conflicts:
    	OregonTrail/src/oregontrail/OregonTrail.java

--- OregonTrail/src/oregontrail/OregonTrail.java
+++ OregonTrail/src/oregontrail/OregonTrail.java
@@ -54,7 +54,7 @@ public class OregonTrail {
     }
     
     public static void setPlayer(Player player) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.player = player; //To change body of generated methods, choose Tools | Templates.
     }
 
     public static Game getCurrentGame() {
