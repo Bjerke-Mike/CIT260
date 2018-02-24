72211838e99945b3c4d4cde8043baaefbdeb7357
bje16002 <36021778+bje16002@users.noreply.github.com>
2018-02-23 18:44:23 -0800

    01 Feb 2018 : Created project. 02 Feb 2018 : Modified Project

--- OregonTrail/src/oregontrail/view/MainMenuView.java
+++ OregonTrail/src/oregontrail/view/MainMenuView.java
@@ -5,16 +5,59 @@
  */
 package oregontrail.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Mike
  */
 class MainMenuView {
     public void displayMainMenuView() {
-
-        System.out.println("\tN - Start new game\n" +
+        boolean endView = false;
+        do {
+            char inputChar = getInputChar();
+        }
+        while (endView = false);
+    }
+    
+    private char getInputChar() {
+       
+       String inputs = null;
+       char inputChar;
+       inputChar = ' ';
+       boolean valid = false;
+       while(valid == false) {
+            displayBanner();
+            Scanner sc = new Scanner(System.in);
+            inputs = sc.nextLine();  //Get the value entered from the keyboard
+            
+            inputs = inputs.trim();
+            
+            if (inputs.length() < 1) {
+                System.out.println("You must enter a non-blank value.");
+                continue;
+            }
+            else {
+                //Converts the string to just the first character
+                inputs = inputs.toUpperCase();
+                inputChar = inputs.charAt(0);
+                if (inputChar == 'N' || inputChar == 'R' || inputChar == 'H' ||
+                        inputChar == 'E') {
+                    valid = true;
+                }
+                else {
+                    System.out.println("You must enter N, R, H, or E.");
+                }
+            }
+        } 
+        return inputChar;
+    }
+    
+    private void displayBanner(){
+        System.out.println("\n\tN - Start new game\n" +
                            "\tR - Restart existing game\n" +
                            "\tH - Get help on how to play the game\n" +
                            "\tE - Exit");
     }
+    
 }
