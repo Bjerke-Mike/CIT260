d7d3f847d2dbf602e51c63c8643acf1e1d5d2a18
bje16002 <36021778+bje16002@users.noreply.github.com>
2018-02-28 20:17:03 -0800

    01 Feb 2018 : Created project. 02 Feb 2018 : Modified Project

--- OregonTrail/src/oregontrail/view/HuntingView.java
+++ OregonTrail/src/oregontrail/view/HuntingView.java
@@ -18,7 +18,7 @@ public class HuntingView {
             displayBanner1();
             long startTime = System.currentTimeMillis();
             do{
-                ;
+                ; // do nothing while waiting for the timer to expire
             }
             while ((System.currentTimeMillis() - startTime) < 1500); // aprox 1.5 seconds
             displayBanner2();
@@ -28,13 +28,20 @@ public class HuntingView {
             totalTime = System.currentTimeMillis() - startTime;
             inputs = inputs.toLowerCase();
             if (inputs.equals("bang")) {
-                if (totalTime < 2000) { // aprox 2 seconds
+                if (totalTime <= 2000) { // aprox 2 seconds 100 to 200 food
                     System.out.println("You got a big one!\nThere will plenty of food tonight!");
-                    return 200;
+                    if (totalTime < 1000)
+                        return 200; // if you get it in less than 1 second, you get max food.
+                    // otherwise, you lose 1 food for each .01 second longer than that.
+                    return (200 - ((int) (totalTime - 1000) /10));
                 }
-                else if (totalTime < 4000) { // aprox 4 seconds
-                    System.out.println("You got a small one.\nThis won't last very long.");
-                    return 100;
+                else if (totalTime <= 4000) { // aprox 4 seconds, 50 to 100 food
+                    System.out.println("You got a medium one.\nThis won't last very long.");
+                    return (100 - ((int) (totalTime - 2000) / 40));
+                }
+                else if (totalTime <= 6000) { // aprox 6 seconds, 25 to 50 food.
+                    System.out.println("You got a small one.\nYou will need to go hunting again soon.");
+                    return (50 - ((int) (totalTime - 4000) / 80));
                 }
                     
             }
