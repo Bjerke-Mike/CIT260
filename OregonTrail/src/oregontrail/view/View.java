98c14c241b62f0b2ced9dfd943a2f571c9757302
bje16002 <36021778+bje16002@users.noreply.github.com>
2018-03-08 16:07:39 -0800

    01 Feb 2018 : Created project. 02 Feb 2018 : Modified Project

---
+++ OregonTrail/src/oregontrail/view/View.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oregontrail.view;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Mike
+ */
+public abstract class View implements ViewInterface {
+    
+    protected String displayMessage;
+    
+    public View() {
+    }
+    
+    public View(String message) {
+        this.displayMessage = message;
+    }
+    
+    @Override
+    public void display() {
+        boolean done = false;
+        do {
+            // prompt for and get the players name
+            String value = this.getInputs();
+            if (value.toUpperCase().equals("Q")) // user wants to quit
+                return;
+            done = this.doAction(value);
+        }
+        while (!done);
+    }
+    
+    @Override
+    public String getInput() {
+       
+       Scanner keyboard = new Scanner(System.in);
+       boolean valid = false;
+       String value = null;
+       
+       // while a valid name has not been retrieved
+       while (!valid) {
+            
+            // prompt for the player's name
+            System.out.println("\n" + this.displayMessage);
+            
+            // get the value entered from the keyboard
+            value = keyboard.nextLine();
+            value = value.trim();
+            if (value.length() < 1) {
+                System.out.println("You must enter a non-blank value.");
+                continue;
+            }
+            break;
+        }
+        return value; // return the name
+    }
+
+    private char getInputChar() {
+        String inputs = null;
+        char inputChar;
+        inputChar = ' ';
+        boolean valid = false;
+        while(valid == false) {
+            inputs = getInput();
+            //Converts the string to just the first character
+            inputChar = inputs.toUpperCase().charAt(0);
+        } 
+        return inputChar;
+    }
+    
+
+
+}
