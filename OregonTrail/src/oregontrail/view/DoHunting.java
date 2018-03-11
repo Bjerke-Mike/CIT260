a11b9cddef012dc945611dde96648fce043d130f
bje16002 <36021778+bje16002@users.noreply.github.com>
2018-03-10 19:21:40 -0800

    01 Feb 2018 : Created project. 02 Feb 2018 : Modified Project

--- OregonTrail/src/oregontrail/view/DoHunting.java
+++ OregonTrail/src/oregontrail/view/DoHunting.java
@@ -10,7 +10,22 @@ package oregontrail.view;
  * @author Mike
  */
 public class DoHunting extends View {
+    private String wordName;
+
     public DoHunting() {
-        
+        ;
+    }
+    
+    public DoHunting(String wordName) {
+        super("\n\tTYPE:  " + wordName);
+        this.wordName = wordName;
+    }
+
+    @Override
+    public boolean doAction(String value) {
+        boolean wordCheck = false;
+        if (value.equals(wordName))
+            wordCheck = true;
+        return wordCheck;
     }
 }
