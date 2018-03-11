3b56893a7d0e005eec6e193857b5d83a79360650
bje16002 <36021778+bje16002@users.noreply.github.com>
2018-03-10 18:42:05 -0800

    01 Feb 2018 : Created project. 02 Feb 2018 : Modified Project

---
+++ OregonTrail/src/oregontrail/view/DoHunting.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oregontrail.view;
+
+/**
+ *
+ * @author Mike
+ */
+public class DoHunting extends View {
+    public DoHunting() {
+        
+    }
+}
