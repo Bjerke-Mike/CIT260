e7d59c56f24c7cc3d5516a8ea94fbd84b578d45a
mellissah. <mellissah.@192.168.0.6>
2018-03-15 20:25:53 -0700

    Merge origin/master
    Conflicts:
    	OregonTrail/src/oregontrail/OregonTrail.java

--- OregonTrail/src/oregontrail/view/GameMenuView.java
+++ OregonTrail/src/oregontrail/view/GameMenuView.java
@@ -9,8 +9,66 @@ package oregontrail.view;
  *
  * @author mellissah.
  */
-public class GameMenuView {
-    public void displayGameMenuView() {
-        
+public class GameMenuView extends View {
+    
+    public GameMenuView() {
+        super("\n\tV - View Map\n" +
+              "\tI - View list of items in inventory.\n" +
+              "\tM - Move on down the trail.\n" +
+              "\tF - Hunt for food.\n" +
+              "\tH - Help\n" +
+              "\tQ - Quit");
     }
+    
+    @Override
+    public boolean doAction(String value) {
+        value = value.toUpperCase(); //convert all uppercase
+        char inputChar; 
+        boolean endView = false;
+        
+        inputChar = getInputChar(value);
+        
+        switch (inputChar) {
+            case 'V': 
+               displayMap(); 
+               break;
+            case 'I':
+               displayInventory();
+               break;
+            case 'M':
+                displayMove();
+                break;
+            case 'F':
+                goHunting();
+                break;
+            case 'H':
+               displayHelp();
+               break;    
+            case 'Q':
+                return true;
+            default: 
+                System.out.println("Invalid menu item.");
+        }
+        return false; 
+    }
+
+    private void goHunting() {
+        HuntingView startHunting = new HuntingView();
+        int foodReceived = 0;
+        foodReceived = startHunting.displayHuntingView();
+        System.out.println("You received " + foodReceived + " food.");
+    }
+    private void displayMap(){
+        System.out.println("*** displayMap() called***");
+    } 
+    private void displayMove(){
+        System.out.println("*** displayMove() called***");
+    }
+    private void displayInventory(){
+        System.out.println("*** displayInventory() called***");
+    }
+    private void displayHelp(){
+        System.out.println("*** displayHelp() called***");
+    }
+  
 }
