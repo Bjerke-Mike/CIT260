3e42463dfc7783982ac944160b8f1045ff6ee777
bje16002 <36021778+bje16002@users.noreply.github.com>
2018-03-22 20:02:54 -0700

    01 Feb 2018 : Created project. 02 Feb 2018 : Modified Project

--- OregonTrail/src/oregontrail/control/MapControl.java
+++ OregonTrail/src/oregontrail/control/MapControl.java
@@ -6,14 +6,20 @@
 package oregontrail.control;
 
 import oregontrail.OregonTrail;
+import oregontrail.exceptions.MapControlException;
+import oregontrail.model.Actor;
+import oregontrail.model.Game;
 import oregontrail.model.Map;
+import oregontrail.model.Location;
+import oregontrail.model.Locations;
 
 /**
  *
  * @author mellissah.
  */
 public class MapControl {
-    
+    private static int currentRow = 1;
+    private static int currentColumn = 1;
     public static Map createMap(int numOfRows, int numOfColumns) {
         if (numOfRows < 0 || numOfColumns < 0) {
             return null;
@@ -39,5 +45,41 @@ public class MapControl {
         Map.setNumOfColumns(numOfColumns);
         return map;
     }
+    
+    public static Location moveActor(Actor actor, int newRow, int newColumn)
+                                     throws MapControlException {
+        if (actor == null){
+            throw new MapControlException("No actor to move.");
+        }
+        Game game = OregonTrail.getCurrentGame();
+        Map map = OregonTrail.getMap();
+        // This is an enum class, so I can't instance it. I will try to fix it later.
+        //Locations locations = new Locations();
+        if (newRow < 1 || newRow > map.getNumOfRows() || 
+            newColumn < 1 || newColumn > map.getNumOfRows()) {
+            throw new MapControlException("Can't move off the map.");
+        }
+        int oldRow = currentRow;
+        int oldColumn = currentColumn;
+        
+        return null;
+    }
+
+    public static int getCurrentRow() {
+        return currentRow;
+    }
+
+    public static void setCurrentRow(int currentRow) {
+        MapControl.currentRow = currentRow;
+    }
+
+    public static int getCurrentColumn() {
+        return currentColumn;
+    }
+
+    public static void setCurrentColumn(int currentColumn) {
+        MapControl.currentColumn = currentColumn;
+    }
+    
     
 }
