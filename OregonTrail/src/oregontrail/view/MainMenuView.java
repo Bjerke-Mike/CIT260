cc77cac248661ae7294f5007d7d17a4e73f3c90d
mellissah. <mellissah.@192.168.0.6>
2018-03-22 17:13:37 -0700

    Merge origin/master
    Conflicts:
    	OregonTrail/src/oregontrail/OregonTrail.java

--- OregonTrail/src/oregontrail/view/MainMenuView.java
+++ OregonTrail/src/oregontrail/view/MainMenuView.java
@@ -46,7 +46,7 @@ public class MainMenuView extends View {
                 startNewGame();
             } catch (GameControlException e) {
                 System.out.println(e.getMessage);
-                return true;
+                return false;
             }
         }
                break;
