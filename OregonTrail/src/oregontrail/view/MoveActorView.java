4071664b81a053918246a702565a1f8c93845f7d
bje16002 <36021778+bje16002@users.noreply.github.com>
2018-03-22 21:46:23 -0700

    01 Feb 2018 : Created project. 02 Feb 2018 : Modified Project

--- OregonTrail/src/oregontrail/view/MoveActorView.java
+++ OregonTrail/src/oregontrail/view/MoveActorView.java
@@ -5,10 +5,44 @@
  */
 package oregontrail.view;
 
+import oregontrail.OregonTrail;
+import oregontrail.model.Player;
+
 /**
  *
  * @author Mike
  */
-public class MoveActorView {
+public class MoveActorView extends View {
+
+    public MoveActorView() {
+        super("\n\tEnter a number row followed by a column number.");
+    }
     
+    private boolean doAction(String [] inputs) {
+        int row;
+        int column;
+        try{
+            row = Integer.parseInt(inputs[0]);
+            column = Integer.parseInt(inputs[1]);
+        } catch (NumberFormatException e){
+            System.out.println("The row and column must be a number");
+            return false;
+        }
+        Player player = OregonTrail.getPlayer();
+        
+        return true;
+    }
+    
+    private String[] getInputs() {
+        String[] inputs = new String[2];
+        inputs[0] = getInput();
+        inputs[1] = getInput();
+        
+        return inputs;
+    }
+
+    @Override
+    public boolean doAction(String value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
