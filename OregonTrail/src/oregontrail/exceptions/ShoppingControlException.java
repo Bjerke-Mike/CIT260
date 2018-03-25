913454c8172ee64b38677631bd8fc12cb4b31d40
bje16002 <36021778+bje16002@users.noreply.github.com>
2018-03-24 20:29:45 -0700

    01 Feb 2018 : Created project. 02 Feb 2018 : Modified Project

--- OregonTrail/src/oregontrail/exceptions/ShoppingControlException.java
+++ OregonTrail/src/oregontrail/exceptions/ShoppingControlException.java
@@ -11,6 +11,8 @@ package oregontrail.exceptions;
  */
 public class ShoppingControlException extends Exception {
 
+    public boolean getMessage;
+    
     public ShoppingControlException() {
     }
 
@@ -29,5 +31,5 @@ public class ShoppingControlException extends Exception {
     public ShoppingControlException(String string, Throwable thrwbl, boolean bln, boolean bln1) {
         super(string, thrwbl, bln, bln1);
     }
-    
+
 }
