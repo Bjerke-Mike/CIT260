bc8e9901f751f09a535f09196b21f590c99c61e0
mellissah. <mellissah.@192.168.0.6>
2018-03-29 18:44:29 -0700

    Merge origin/master
    Conflicts:
    	OregonTrail/src/oregontrail/OregonTrail.java

--- OregonTrail/src/oregontrail/view/HuntingView.java
+++ OregonTrail/src/oregontrail/view/HuntingView.java
@@ -33,29 +33,35 @@ public class HuntingView {
         totalTime = System.currentTimeMillis() - startTime;
         if (huntingSuccess) {
             if (totalTime <= 2000) { // aprox 2 seconds 100 to 200 food
-                System.out.println("You got a big one!\nThere will plenty of food tonight!");
+                ErrorView.display(this.getClass().getName(),
+                          "You got a big one! \nThere will be plentry of food tonight!");
                 if (totalTime < 1000)
                     return 200; // if you get it in less than 1 second, you get max food.
                 // otherwise, you lose 1 food for each .01 second longer than that.
                 return (200 - ((int) (totalTime - 1000) /10));
             }
             else if (totalTime <= 4000) { // aprox 4 seconds, 50 to 100 food
-                System.out.println("You got a medium one.\nThis won't last very long.");
+                ErrorView.display(this.getClass().getName(),
+                          "You got a medium one. \nThis won't last very long.");
                 return (100 - ((int) (totalTime - 2000) / 40));
             }
             else if (totalTime <= 6000) { // aprox 6 seconds, 25 to 50 food.
-                System.out.println("You got a small one.\nYou will need to go hunting again soon.");
+                ErrorView.display(this.getClass().getName(),
+                          "You got a small one. \nYou will need to go hunting again soon.");
                 return (50 - ((int) (totalTime - 4000) / 80));
             }
         }
         // Longer than 6 seconds or mistyped "bang"
-        System.out.println("You missed it!" + 
-                "\nI hope you have food in your wagon or you may starve.");
+        ErrorView.display(this.getClass().getName(),
+                          "You missed it!"
+                           + "\nI hope you have food in your wagon or you may starve.");
         return 0;
     }
 
     private void displayBanner(){
         System.out.println("\n\tYou go hunting.");
+        ErrorView.display(this.getClass().getName(),
+                          "\n\tYou go hunting.");
     }
     
 }
