cea5a9bd5ac4a86f3245e4c7749bc5da29acc548
mellissah. <mellissah.@192.168.0.6>
2018-03-29 18:44:18 -0700

    Merge origin/master
    Conflicts:
    	OregonTrail/src/oregontrail/OregonTrail.java

---
+++ OregonTrail/src/oregontrail/view/ErrorView.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oregontrail.view;
+
+import java.io.PrintWriter;
+import oregontrail.OregonTrail;
+
+/**
+ *
+ * @author mellissah.
+ */
+public class ErrorView {
+    public static PrintWriter console = OregonTrail.getOutput();
+    public static PrintWriter log = OregonTrail.getLogFile();
+    
+    public static void display(String className, String errorMessage) {
+        console.println(
+                "/n--- ERROR ----------------------"
+              + "/n" + errorMessage
+              + "/n--------------------------------");
+        
+        log.printf("%n%n%s", className + " - " + errorMessage);
+    }
+    
+}
