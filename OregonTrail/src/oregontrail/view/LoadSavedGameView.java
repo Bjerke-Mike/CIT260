9c74555706b1894b56028d02ec2ae0c086d89d72
bje16002 <36021778+bje16002@users.noreply.github.com>
2018-03-30 20:27:18 -0700

    Updated some typos.

---
+++ OregonTrail/src/oregontrail/view/LoadSavedGameView.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oregontrail.view;
+
+import java.io.IOException;
+import oregontrail.OregonTrail;
+import oregontrail.control.GameControl;
+import oregontrail.exceptions.GameControlException;
+import oregontrail.model.Game;
+
+/**
+ *
+ * @author Mike
+ */
+public class LoadSavedGameView extends View {
+
+    public LoadSavedGameView() {
+        super("\n\t  Enter the name of the file to load:");
+    }
+
+    @Override
+    public boolean doAction(String value) {
+        Game game = OregonTrail.getCurrentGame();
+        try {
+            GameControl.getGame(game, value);
+        } catch (GameControlException e) {
+            this.console.println(e.getMessage());
+            return false;
+        } catch (IOException e) {
+            this.console.println("I/O Error: " + e.getMessage());
+            return false;
+        }
+        this.console.println("Game successfully saved");
+        
+        
+        return true;
+    }
+    
+}
