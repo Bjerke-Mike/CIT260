7e8e5a2f50fdb92cee3c4d8da032dad42092325a
bje16002 <36021778+bje16002@users.noreply.github.com>
2018-03-31 11:41:51 -0700

    Updated some typos.

--- OregonTrail/src/oregontrail/view/ShoppingControlView.java
+++ OregonTrail/src/oregontrail/view/ShoppingControlView.java
@@ -14,14 +14,64 @@ import oregontrail.exceptions.ShoppingControlException;
  * @author mellissah.
  */
 public class ShoppingControlView extends View {
-
-    public ShoppingControlView() {
-        super("\n\tEnter a position and inventory item.");
+    ShoppingControl shopping = new ShoppingControl();
+    
+    public ShoppingControlView(String location) throws ShoppingControlException {
+        super(  "\n"
+              + "\n\t-----------------------------------------"
+              + "\n\t  Shopping at " + location
+              + "\n\t-----------------------------------------"
+              + "\n\t  C - Change location"
+              + "\n\t  P - Show prices"
+              + "\n\t  S - Save prices to a file"
+              + "\n\t  H - Get help on this menu"
+              + "\n\t  E - Exit this menu."
+              + "\n\t-----------------------------------------");
     }
         
     @Override
     public boolean doAction(String value) {
-        return true;
+        value = value.toUpperCase(); //convert all uppercase
+        char inputChar; 
+        boolean endView = false;
+        
+        inputChar = getInputChar(value);
+        
+        switch (inputChar) {
+            case 'C':
+               this.changeLocation();
+               break;
+            case 'P':
+                this.showPrices();
+                break;
+            case 'L':
+                this.savePrices();
+                break;
+            case 'H':
+                getShoppingHelp();
+                break;
+            case 'E':
+                return true;
+            default: 
+                this.console.println("Invalid menu item.");
+        }
+        return false; 
+    }
+
+    private void changeLocation() {
+        this.console.println("changeLocation() in Shopping Control View called.");
+    }
+
+    private void showPrices() {
+        this.console.println("showPrices() in Shopping Control View called.");
+    }
+
+    private void savePrices() {
+        this.console.println("savePrices() in Shopping Control View called.");
+    }
+
+    private void getShoppingHelp() {
+        this.console.println("getShoppingHelp() in Shopping Control View called.");
     }
 
 }
