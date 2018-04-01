b4f97eebf6eadbf1e4d914cbb7aaf23fa922bead
bje16002 <36021778+bje16002@users.noreply.github.com>
2018-03-31 21:18:45 -0700

    Updated some typos.

--- OregonTrail/src/oregontrail/view/ChangeFort.java
+++ OregonTrail/src/oregontrail/view/ChangeFort.java
@@ -5,6 +5,9 @@
  */
 package oregontrail.view;
 
+import oregontrail.OregonTrail;
+import oregontrail.control.ShoppingControl;
+
 /**
  *
  * @author Mike
@@ -18,7 +21,21 @@ public class ChangeFort extends View {
 
     @Override
     public boolean doAction(String value) {
-        this.console.println("changeLocation() in Shopping Control View called.");
+        int newFortNum;
+        try {
+            newFortNum = Integer.parseInt(value);
+        } catch(NumberFormatException e) {
+            console.println("You must enter a number.");
+            return false;
+        }
+        ShoppingControl shopping = new ShoppingControl();
+        int maxNum = shopping.getLocationName().length;
+        if (newFortNum < 1 || newFortNum > maxNum) {
+            console.println("You must enter a number between 1 and " + maxNum);
+            return false;
+        }
+        OregonTrail.setLocationNum(newFortNum - 1);
+        //this.console.println("doAction() in Change Fort " + newFortNum + "called.");
         return true;
     }
     
