15813212b206ebc68c8bceb75a6defb3fb26f47a
bje16002 <36021778+bje16002@users.noreply.github.com>
2018-03-31 20:13:18 -0700

    Updated some typos.

---
+++ OregonTrail/src/oregontrail/view/ChangeFort.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oregontrail.view;
+
+/**
+ *
+ * @author Mike
+ */
+public class ChangeFort extends View {
+
+    @Override
+    public boolean doAction(String value) {
+        this.console.println("changeLocation() in Shopping Control View called.");
+        return true;
+    }
+    
+}
